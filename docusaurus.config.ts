ac17f3821b05aa64983eb48564d7f918ddbeaf24
zbynekzundalek <zbynek.zundalek@gmail.com>
2025-10-12 18:51:56 +0900

    feat: set domain to zodart.mergepanic.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://zzundalek.github.io',
+  url: 'https://zodart.mergepanic.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/zodart_doc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
